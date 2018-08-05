c92eb0935e311d6dc1213465a4e897ace9b843a6
i.lepeskevicius@gmail.com <i.lepeskevicius@gmail.com>
2018-08-05 16:08:28 -0300

    features definition

---
+++ src/test/java/Steps/AccountStep.java
@@ -0,0 +1,69 @@
+package Steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class AccountStep {
+    @Given("^I navigate to the personal information page$")
+    public void iNavigateToThePersonalInformationPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I update my details$")
+    public void iUpdateMyDetails() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I receive feedback that my account is updated$")
+    public void iReceiveFeedbackThatMyAccountIsUpdated() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I change my street name$")
+    public void iChangeMyStreetName() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I navigate to my orders$")
+    public void iNavigateToMyOrders() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I see a list of my orders$")
+    public void iSeeAListOfMyOrders() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I can open an order to see the order details$")
+    public void iCanOpenAnOrderToSeeTheOrderDetails() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click the Log out button$")
+    public void iClickTheLogOutButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I receive feedback that I am logged out$")
+    public void iReceiveFeedbackThatIAmLoggedOut() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I cannot visit my account page anymore$")
+    public void iCannotVisitMyAccountPageAnymore() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
---
+++ src/test/java/Steps/SearchStep.java
@@ -0,0 +1,69 @@
+package Steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SearchStep {
+    @Given("^I am on the homepage to do a single keyword search$")
+    public void iAmOnTheHomepageToDoASingleKeywordSearch() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I do a single keyword search for <keyword>$")
+    public void iDoASingleKeywordSearchForKeyword() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I see a single keyword search result page with more than zero results$")
+    public void iSeeASingleKeywordSearchResultPageWithMoreThanZeroResults() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am on the homepage to do a search with multiple keywords$")
+    public void iAmOnTheHomepageToDoASearchWithMultipleKeywords() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I do a multiple keyword search for <key(\\d+)> <key(\\d+)>$")
+    public void iDoAMultipleKeywordSearchForKeyKey(int arg0, int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I see a multiple keyword search result page with more than zero results$")
+    public void iSeeAMultipleKeywordSearchResultPageWithMoreThanZeroResults() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I have done a search using a valid search term$")
+    public void iHaveDoneASearchUsingAValidSearchTerm() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I see a certain amount of items$")
+    public void iSeeACertainAmountOfItems() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I filter using the size <filter>$")
+    public void iFilterUsingTheSizeFilter() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^The results are filtered to display a smaller amount of items$")
+    public void theResultsAreFilteredToDisplayASmallerAmountOfItems() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
